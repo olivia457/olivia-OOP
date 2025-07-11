fbba5a59ecb7cba9395eed60ba5028b8c6e4ed66
Olivia Jose <olijoseortiz@gmail.com>
2025-07-11 17:39:10 -0600

    primer commit

---
+++ practica 1.py
@@ -0,0 +1,66 @@
+from math import sqrt
+
+print("ingrese los valores del punto 1")
+x1=int(input("x1:"))
+y1=int(input("y1:"))
+print("ingrese los valores del punto 2")
+x2=int(input("x2:"))
+y2=int(input("y2:"))
+
+print("distancia manhattan")
+#resta1
+x2=4
+x1=2
+resta=x2-x1
+print(f"la resta de {x2} y {x1} es: {resta}")
+ #resta2
+y2=8
+y1=3
+resta=y2-y1
+print(f"la resta de {y2} y {y1} es: {resta}")
+
+print("la distancia manhattan es: ")
+dx=2
+dy=5
+suma=2+5
+print(f"la suma de {2} y {5} es: {suma}")
+
+
+print("distancia euclidiana")
+print("formula = (x2-x1)**2 + (y2-y1)**2")
+
+#resta1
+x2=4
+x1=2
+resta=x2-x1
+print(f"la resta de {x2} y {x1} es: {resta}")
+ #resta2
+y2=8
+y1=3
+resta=y2-y1
+print(f"la resta de {y2} y {y1} es: {resta}")
+
+print("potencia de dx y dy")
+dx=2
+dy=5
+potencia = 2**2,5**2
+print(potencia)
+
+print("la suma de los dos puntos")
+dx=4
+dy=25
+suma=4+25
+print(f"la suma de {4} y {25} es: {suma}")
+
+print("resultado de la distancia euclidiana es: ")
+de=29
+raiz=sqrt(de)
+print(raiz)
+
+
+
+
+
+ 
+       
+       
